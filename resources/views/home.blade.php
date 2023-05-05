414c2ccb6718f91cbec2c8c0ce1bedbd8ea5c263
JocelynWievin <132711712+JocelynWievin@users.noreply.github.com>
2023-05-06 03:44:33 +0700

    UTS Web Programming

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/home.css">
+
+    <title>Home Page</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <div class="logo">
+                <img src="/images/sbux_logo.png" alt="logo">
+            </div>
+
+            <h1>Starbucks Coffee</h1>
+        </div>
+
+        <div class="container1">
+        <ul class="ul-li-container1">
+            <li class="profile"><a href="home" class="fontstyle1">Profile</a></li>
+            <li class="orders"><a href="orders" class="fontstyle1">Transaction</a></li>
+            <hr />
+        </ul>
+    </div>
+
+    <hr class="sep">
+
+    </header>
+
+    <div class="points-content">
+        <img src="/images/pcircle.png" alt="p">
+        <h2 class="pointnum">100</h2>
+        <p class="points">Points</p>
+    </div>
+
+    <img src="/images/sbux_cardd.png" alt="card" class="card">
+
+    <div class="promos">
+        <h3>Promotions</h3>
+    </div>
+
+    <div class="promo-card">
+        <img src="/images/promo1.jpg" alt="promo1" class="promo-img">
+
+        <div class="promo-content">
+            <h4>Today's Promo</h4>
+            <p>Bukber Special</p>
+        </div>
+
+    </div>
+
+ </body>
+</html>
